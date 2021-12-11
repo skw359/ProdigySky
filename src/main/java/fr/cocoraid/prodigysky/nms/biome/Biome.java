97ee71163bb65fa56678b15e8b56489ae082e27c
Wizz <75508453+skw359@users.noreply.github.com>
2021-12-10 21:16:29 -0500

    more stuff

--- src/main/java/fr/cocoraid/prodigysky/nms/biome/Biome.java
+++ src/main/java/fr/cocoraid/prodigysky/nms/biome/Biome.java
@@ -3,8 +3,7 @@ package fr.cocoraid.prodigysky.nms.biome;
 import net.minecraft.world.level.biome.BiomeBase;
 
 public interface Biome {
+    BiomeBase build(String var1, String var2, String var3, String var4, String var5, String var6);
 
-     BiomeBase build(String fogColor, String waterColor, String waterFogColor, String skyColor,String grassColor, String foliageColor);
-
-    BiomeBase build(String fogColor,String waterColor,String waterFogColor,String skyColor);
+    BiomeBase build(String var1, String var2, String var3, String var4);
 }
--- src/main/java/fr/cocoraid/prodigysky/nms/biome/BiomeBaseWrapper_1_17R1.java
+++ src/main/java/fr/cocoraid/prodigysky/nms/biome/BiomeBaseWrapper_1_17R1.java
@@ -1,70 +1,60 @@
 package fr.cocoraid.prodigysky.nms.biome;
 
-import com.mojang.serialization.Codec;
-import net.minecraft.data.worldgen.BiomeDecoratorGroups;
-import net.minecraft.data.worldgen.WorldGenSurfaceComposites;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import net.minecraft.world.level.biome.BiomeBase;
-import net.minecraft.world.level.biome.BiomeFog;
-import net.minecraft.world.level.biome.BiomeSettingsGeneration;
 import net.minecraft.world.level.biome.BiomeSettingsMobs;
-import net.minecraft.world.level.levelgen.WorldGenStage;
+import net.minecraft.world.level.biome.BiomeBase.Geography;
+import net.minecraft.world.level.biome.BiomeBase.Precipitation;
+import net.minecraft.world.level.biome.BiomeBase.TemperatureModifier;
+import net.minecraft.world.level.biome.BiomeSettingsGeneration.a;
+import net.minecraft.world.level.levelgen.WorldGenStage.Decoration;
 
 public class BiomeBaseWrapper_1_17R1 implements Biome {
+    private String fogColor;
+    private String waterColor;
+    private String waterFogColor;
+    private String skyColor;
+    private String grassColor;
+    private String foliageColor;
 
-
-    private String fogColor, waterColor, waterFogColor, skyColor;
-    private String grassColor,foliageColor;
-
-    @Override
-    public BiomeBase build(String fogColor, String waterColor, String waterFogColor, String skyColor) {
-        return build(fogColor,waterColor,waterFogColor,skyColor,null,null);
+    public BiomeBaseWrapper_1_17R1() {
     }
 
-    @Override
-    public BiomeBase build(String fogColor, String waterColor, String waterFogColor, String skyColor,String grassColor, String foliageColor) {
+    public BiomeBase build(String fogColor, String waterColor, String waterFogColor, String skyColor) {
+        return this.build(fogColor, waterColor, waterFogColor, skyColor, (String)null, (String)null);
+    }
+
+    public BiomeBase build(String fogColor, String waterColor, String waterFogColor, String skyColor, String grassColor, String foliageColor) {
         this.fogColor = fogColor;
         this.waterColor = waterColor;
         this.waterFogColor = waterFogColor;
         this.skyColor = skyColor;
         this.grassColor = grassColor;
         this.foliageColor = foliageColor;
-        return build();
+        return this.build();
     }
-
-
 
     public BiomeBase build() {
-        //void generation
-        BiomeSettingsGeneration.a gen = (new BiomeSettingsGeneration.a()).a(WorldGenSurfaceComposites.p);
-        gen.a(WorldGenStage.Decoration.j, BiomeDecoratorGroups.W);
+        a gen = new a();
 
-        BiomeFog.a biomeFogCodec = new BiomeFog.a()
-                .a(Integer.parseInt(fogColor, 16)) //fog color
-                .b(Integer.parseInt(waterColor, 16)) //water color
-                .c(Integer.parseInt(waterFogColor, 16)) //water fog color
-                .d(Integer.parseInt(skyColor, 16)); //skycolor
-                //.e() //foliage color (leaves, fines and more)
-                //.f() //grass blocks color
-                //.a(BiomeParticle)
-                //a(Music)
+        try {
+            Method method = gen.getClass().getDeclaredMethod("a", Integer.TYPE);
+            method.setAccessible(true);
+            method.invoke(gen, Decoration.j.ordinal());
+        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException var3) {
+            var3.printStackTrace();
+        }
 
-        if(foliageColor != null)
-            biomeFogCodec.e(Integer.parseInt(foliageColor, 16));
-        if(grassColor != null)
-            biomeFogCodec.f(Integer.parseInt(grassColor, 16));
+        net.minecraft.world.level.biome.BiomeFog.a biomeFogCodec = (new net.minecraft.world.level.biome.BiomeFog.a()).a(Integer.parseInt(this.fogColor, 16)).b(Integer.parseInt(this.waterColor, 16)).c(Integer.parseInt(this.waterFogColor, 16)).d(Integer.parseInt(this.skyColor, 16));
+        if (this.foliageColor != null) {
+            biomeFogCodec.e(Integer.parseInt(this.foliageColor, 16));
+        }
 
-        return new BiomeBase.a()
-                .a(BiomeBase.Precipitation.a) //none
-                .a(BiomeBase.Geography.a) //none
-                .a(0F) //depth ocean or not // var3 ? -1.8F : -1.0F
-                .b(0F) //scale Lower values produce flatter terrain
-                .c(0F) //temperature
-                .d(0F) //maybe important, foliage and grass color
-                .a(biomeFogCodec.a()) //biomefog
-                .a(BiomeSettingsMobs.c) //same as void biome
-                .a(gen.a()) //same as void biome
-                .a();
+        if (this.grassColor != null) {
+            biomeFogCodec.f(Integer.parseInt(this.grassColor, 16));
+        }
+
+        return (new net.minecraft.world.level.biome.BiomeBase.a()).a(Precipitation.a).a(Geography.a).a(0.0F).b(0.0F).a(TemperatureModifier.a).a(biomeFogCodec.a()).a(BiomeSettingsMobs.c).a(gen.a()).a();
     }
-
-
 }
