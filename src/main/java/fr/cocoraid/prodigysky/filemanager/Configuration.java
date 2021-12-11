072f5f1ae5e7584117e34f5fd7e266c64d5d4716
Wizz <75508453+skw359@users.noreply.github.com>
2021-12-10 21:19:16 -0500

    Update Configuration.java

--- src/main/java/fr/cocoraid/prodigysky/filemanager/Configuration.java
+++ src/main/java/fr/cocoraid/prodigysky/filemanager/Configuration.java
@@ -2,6 +2,15 @@ package fr.cocoraid.prodigysky.filemanager;
 
 import com.mojang.serialization.Lifecycle;
 import fr.cocoraid.prodigysky.ProdigySky;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.minecraft.core.IRegistry;
 import net.minecraft.core.IRegistryWritable;
 import net.minecraft.resources.MinecraftKey;
@@ -14,98 +23,77 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.craftbukkit.v1_17_R1.CraftServer;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.bukkit.craftbukkit.v1_18_R1.CraftServer;
 
 public class Configuration {
-
-    private Map<String, Integer> biomes = new HashMap<>();
+    private Map<String, Integer> biomes = new HashMap();
     private static final Pattern HEX_PATTERN = Pattern.compile("^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
-
-    private List<World> enabledWorlds = new ArrayList<>();
-
+    private List<World> enabledWorlds = new ArrayList();
     private ProdigySky instance;
+    private static final String ENABLED_WORLD = "enabled-worlds";
+
     public Configuration(ProdigySky plugin) {
         this.instance = plugin;
     }
 
     public Configuration init() {
-        File file = new File(instance.getDataFolder(), "configuration.yml");
+        File file = new File(this.instance.getDataFolder(), "configuration.yml");
         if (!file.exists()) {
-            instance.saveResource("configuration.yml", false);
-
+            this.instance.saveResource("configuration.yml", false);
         }
+
         return this;
     }
 
-
-    private static final String ENABLED_WORLD = "enabled-worlds";
     public void load() {
         ConsoleCommandSender cc = Bukkit.getConsoleSender();
-
-        File file = new File(instance.getDataFolder() , "configuration.yml");
+        File file = new File(this.instance.getDataFolder(), "configuration.yml");
         FileConfiguration data = YamlConfiguration.loadConfiguration(file);
+        if (data.isSet("enabled-worlds")) {
+            List<String> worlds = data.getStringList("enabled-worlds");
+            Iterator var5 = worlds.iterator();
 
-        if(data.isSet(ENABLED_WORLD)) {
-            List<String> worlds = data.getStringList(ENABLED_WORLD);
-            for (String world : worlds) {
+            while(var5.hasNext()) {
+                String world = (String)var5.next();
                 World w = Bukkit.getWorld(world);
-                if(w  == null) {
-                    cc.sendMessage("§6[ProdigySky Warning] §4 " + world + " §cdoes not exist for enabled-world in configuration.yml");
-                    continue;
+                if (w == null) {
+                    cc.sendMessage("Â§6[ProdigySky Warning] Â§4 " + world + " Â§cdoes not exist for enabled-world in configuration.yml");
+                } else {
+                    this.enabledWorlds.add(w);
                 }
-                enabledWorlds.add(w);
             }
         }
 
-        DedicatedServer ds = ((CraftServer)Bukkit.getServer()).getHandle().getServer();
+        DedicatedServer ds = ((CraftServer)Bukkit.getServer()).getHandle().b();
         ConfigurationSection section = data.getConfigurationSection("custom_colors");
+        if (data.isSet("custom_colors")) {
+            Iterator var21 = section.getKeys(false).iterator();
 
-        if(data.isSet("custom_colors")) {
-            for (String colorName : section.getKeys(false)) {
-                if (biomes.containsKey(colorName)) {
-                    Bukkit.getLogger().log(Level.WARNING, "Color name " + colorName + " already exists !");
-                    continue;
+            while(true) {
+                while(var21.hasNext()) {
+                    String colorName = (String)var21.next();
+                    if (this.biomes.containsKey(colorName)) {
+                        Bukkit.getLogger().log(Level.WARNING, "Color name " + colorName + " already exists !");
+                    } else {
+                        ResourceKey<BiomeBase> newKey = ResourceKey.a(IRegistry.aR, new MinecraftKey(colorName));
+                        String key = colorName + ".";
+                        String fog = section.getString(key + "fog");
+                        String sky = section.getString(key + "sky");
+                        String water = section.getString(key + "water");
+                        String waterFog = section.getString(key + "water_fog");
+                        String foliage = section.getString(key + "foliage");
+                        String grass = section.getString(key + "grass");
+                        if (this.isHex(fog) && this.isHex(sky) && this.isHex(water) && this.isHex(waterFog) && (foliage == null || this.isHex(foliage)) && (grass == null || this.isHex(grass))) {
+                            BiomeBase biomeBase = this.instance.getNMS().getBiome().build(fog, water, waterFog, sky, grass, foliage);
+                            IRegistryWritable<BiomeBase> rw = ds.aV().b(IRegistry.aR);
+                            rw.a(newKey, biomeBase, Lifecycle.stable());
+                            int id = ds.aV().d(IRegistry.aR).a(biomeBase);
+                            this.biomes.put(colorName.toLowerCase(), id);
+                        }
+                    }
                 }
-                ResourceKey<BiomeBase> newKey = ResourceKey.a(IRegistry.aO, new MinecraftKey(colorName));
 
-                String key = colorName + ".";
-                String fog = section.getString(key + "fog");
-                String sky = section.getString(key + "sky");
-                String water = section.getString(key + "water");
-                String waterFog = section.getString(key + "water_fog");
-                String foliage = section.getString(key + "foliage");
-                String grass = section.getString(key + "grass");
-
-                if (!isHex(fog)) continue;
-                if (!isHex(sky)) continue;
-                if (!isHex(water)) continue;
-                if (!isHex(waterFog)) continue;
-
-                if (foliage != null && !isHex(foliage)) continue;
-                if (grass != null && !isHex(grass)) continue;
-
-
-                BiomeBase biomeBase = instance.getNMS().getBiome()
-                        .build(fog,
-                                water,
-                                waterFog,
-                                sky,
-                                grass,
-                                foliage);
-
-                IRegistryWritable<BiomeBase> rw = ds.getCustomRegistry().b(IRegistry.aO);
-                rw.a(newKey, biomeBase, Lifecycle.stable());
-                int id = ds.getCustomRegistry().d(IRegistry.aO).getId(biomeBase);
-                biomes.put(colorName.toLowerCase(), id);
+                return;
             }
         }
     }
@@ -113,16 +101,18 @@ public class Configuration {
     private boolean isHex(String hex) {
         Matcher matcher = HEX_PATTERN.matcher(hex);
         boolean match = matcher.matches();
-        if(!match) Bukkit.getLogger().log(Level.WARNING, " Color with hex " + hex + " is invalid");
+        if (!match) {
+            Bukkit.getLogger().log(Level.WARNING, " Color with hex " + hex + " is invalid");
+        }
+
         return match;
     }
 
-
     public Map<String, Integer> getBiomes() {
-        return biomes;
+        return this.biomes;
     }
 
     public List<World> getEnabledWorlds() {
-        return enabledWorlds;
+        return this.enabledWorlds;
     }
 }
